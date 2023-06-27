e4d17c77cc51c38e8592b435b12b852f5d02ff5d
Amerta666 <beponuka666@gmail.com>
2023-06-27 20:02:25 +0500

    problem_9 solved

--- Seminars/problem_9.java
+++ Seminars/problem_9.java
@@ -1,13 +1,27 @@
-import java.io.FileWriter; // Импорт файлрайтера
+/*
+Записать в файл следующие данные:
+
+Name Surname Age
+Kate Smith 20
+Paul Green 25
+Mike Black 23
+
+ */
+
+
+import java.io.FileWriter;
 import java.io.IOException;
 public class problem_9 {
     public static void main(String[] args) {
-        try (FileWriter fw = new FileWriter("file.txt", false)) {
-            fw.write("line 1");
+        try (FileWriter fw = new FileWriter("Seminars/problem_9.txt", false)) {
             fw.append('\n');
-            fw.append('2');
+            fw.write("Name Surname Age");
             fw.append('\n');
-            fw.write("line 3");
+            fw.write("Kate Smith 20");
+            fw.append('\n');
+            fw.write("Paul Green 25");
+            fw.append('\n');
+            fw.write("Mike Black 23");
             fw.flush();
         } catch (IOException ex) {        // Обработка ошибки(если есть) и её вывод
             System.out.println(ex.getMessage());
