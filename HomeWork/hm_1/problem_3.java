b7a304b498e0f0b2240374cde4e88baa2ec18a31
Amerta666 <beponuka666@gmail.com>
2023-06-27 22:46:04 +0500

    problem_3 solved

--- HomeWork/hm_1/problem_3.java
+++ HomeWork/hm_1/problem_3.java
@@ -4,8 +4,36 @@
 
 
 package hm_1;
+import java.util.Scanner;
+
 public class problem_3 {
     public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int firstNum = iScanner.nextInt();
+        System.out.print("Введите операцию(+ - / *): ");
+        char operation;
+        operation = iScanner.next().charAt(0);
+        System.out.print("Введите второе число: ");
+        int secondNum = iScanner.nextInt();
+        iScanner.close();
+        double rezuls;
+        if (operation == '+') {
+            rezuls = firstNum + secondNum;
+            System.out.printf("%d + %d = %.0f", firstNum, secondNum, rezuls);
+        }
+        else if (operation == '-') {
+            rezuls = firstNum - secondNum;
+            System.out.printf("%d - %d = %.0f", firstNum, secondNum, rezuls);
+        }
+        else if (operation == '*') {
+            rezuls = firstNum * secondNum;
+            System.out.printf("%d * %d = %.0f", firstNum, secondNum, rezuls);
+        }
+        else if (operation == '/') {
+            rezuls = (double) firstNum / secondNum;
+            System.out.printf("%d / %d = %.2f", firstNum, secondNum, rezuls);
+        }
 
     }
 }
