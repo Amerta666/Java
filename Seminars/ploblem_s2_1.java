8d96e7fa2f1b005513d916951dadbfc89003d96f
Amerta666 <beponuka666@gmail.com>
2023-06-29 23:13:53 +0500

    Improved code in problem_s2_1

--- Seminars/ploblem_s2_1.java
+++ Seminars/ploblem_s2_1.java
@@ -1,22 +1,23 @@
 /*
 Дано четное число N (>0) и символы c1 и c2.
-Написать метод, который вернет строку длины N, которая состоит
+Написать метод, который вернет строку, длины N, которая состоит
 из чередующихся символов c1 и c2, начиная с c1.
  */
 
 public class ploblem_s2_1 {
     public static void main(String[] args) {
-        int N = 111;
-        String c1 = ":";
-        String c2 = "=";
+        int N = 11;
+        String firstSymbol = "c1";
+        String secondSymbol = "c2";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N/2; i++) {
-            sb.append(c1);
-            sb.append(c2);
+            sb.append(firstSymbol);
+            sb.append(secondSymbol);
         }
         if (N % 2 != 0) {
-            sb.append(c1);
+            sb.append(firstSymbol);
         }
         System.out.println(sb);
+        System.out.println(sb.length());
     }
 }
