d8fead47e4a7857d6b40a63871115d23d59b6714
Amerta666 <beponuka666@gmail.com>
2023-07-11 23:31:17 +0500

    Add problem_1

---
+++ HomeWork/hm_5/problem_1.java
@@ -0,0 +1,26 @@
+
+//Реализуйте структуру телефонной книги с помощью HashMap,
+//учитывая, что 1 человек может иметь несколько телефонов.
+
+package hm_5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class problem_1 {
+    public static void main(String[] args) {
+        Map<String, int[]> PhoneBook = new HashMap<>();
+        PhoneBook.put("Иванов", new int[]{456496, 4514531, 4534531});
+        PhoneBook.put("Петров", new int[]{456486, 4564531});
+        PhoneBook.put("Сидоров", new int[]{456486});
+        PhoneBook.put("Петрова", new int[]{456486});
+        for (var item : PhoneBook.entrySet()) {
+            String phones = "";
+            for (int i : item.getValue()) {
+                phones = phones + i + " ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+}
+
--- Seminars/problem_s5_2.java
+++ Seminars/problem_s5_2.java
@@ -8,9 +8,31 @@
 // Пример 6: {a+]}{(d*3)} - ложь
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class problem_s5_2 {
     public static void main(String[] args) {
+        String Example  = "{a}[+]{(d*3)}";
+        System.out.println(Example);
+        Map<Character, Integer> db = new HashMap<>();
+        for (int i = 0; i < Example.length(); i++) {
+            if (db.containsKey(Example.charAt(i))) {
+                db.put(Example.charAt(i), db.get(Example.charAt(i)) + 1);
+            }
+            else {
+                db.put(Example.charAt(i), 1);
+            }
+        }
+        if ((Objects.equals(db.get('{'), db.get('}'))) && (db.get('(').equals(db.get(')')))
+                && (db.get('[').equals(db.get(']')))){
+            System.out.println("Запись верная");
+        }
+        else {
+            System.out.println("Запись не верная");
+        }
+
 
     }
 }
